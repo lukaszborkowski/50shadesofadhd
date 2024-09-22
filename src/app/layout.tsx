36731f27edcba4d7caffe0fbefda7a6c66dc73c9
lukas <l.borkowski@madkom.pl>
2024-09-22 14:42:42 +0200

    adjusts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 
 import "./globals.css";
 import { Navbar } from "@/components/sections/Navbar";
@@ -12,7 +13,8 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "Quick ADHD Self-Test",
-  description: "Do you know your ADHD type? Discover common symptoms of ADHD. Early recognition can make all the difference. Start now",
+  description:
+    "Do you know your ADHD type? Discover common symptoms of ADHD. Early recognition can make all the difference. Start now",
 };
 
 export default function RootLayout({
@@ -24,11 +26,25 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.svg" />
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=AW-16710782969`}
+          strategy="afterInteractive" // Wstaw skrypt po interakcji użytkownika
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-16710782969');
+        `}
+        </Script>
       </head>
       <body className={poppins.className}>
         <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
